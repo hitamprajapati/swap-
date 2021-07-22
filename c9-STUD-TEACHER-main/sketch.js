93a9619f232c91ac218b28b4ae2318a9f892a063
hitamprajapati <87807545+hitamprajapati@users.noreply.github.com>
2021-07-22 15:30:00 +0530

    Add files via upload

---
+++ c9-STUD-TEACHER-main/sketch.js
@@ -0,0 +1,33 @@
+
+var a = prompt('Enter the first variable: ');
+
+var b = prompt("enter the second var :") ;
+
+function setup(){
+  var b2 = createButton("click here to Swap");
+b2.mousePressed(swap)
+  }
+
+
+function draw()
+{
+ 
+  
+}
+
+function swap(){
+[a,b]=[b,a]
+console.log("the value of a " +a)
+console.log("the value of b " +b)
+
+}
+
+
+
+
+
+
+
+
+
+         
